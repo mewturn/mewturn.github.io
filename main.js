fa2b0c2e659b0b4ac570fbe6627628c72262512a
mewturn <pathonice@hotmail.com>
2015-01-27 15:48:12 +0800

    Added factories

--- main.js
+++ main.js
@@ -8,6 +8,9 @@ var grandmasCps = 5;
 var bakerys = 0;
 var bakerysCps = 30;
 
+var factories = 0;
+var factoriesCps = 1000;
+
 function cookieClick(number) {
 	cookies = cookies + number;
 	updateResources();
@@ -37,6 +40,13 @@ function buyBakerys() {
 	};
 };
 
+function buyFactories() {
+	if (cookies >= factoriesCost) {
+		factories = factories + 1;
+		cookies = cookies - factoriesCost;
+		reload();
+	};
+};
 
 function save() {
 	var save = {
@@ -46,7 +56,9 @@ function save() {
 		grandmas: grandmas,
 		grandmasCost: grandmasCost,
 		bakerys: bakerys,
-		bakerysCost: bakerysCost
+		bakerysCost: bakerysCost,
+		factories: factories,
+		factoriesCost: factoriesCost
 	};
 	localStorage.setItem("save",JSON.stringify(save)); 
 };
@@ -60,6 +72,8 @@ function load() {
 	if (typeof savegame.grandmasCost !== "undefined") grandmasCost = savegame.grandmasCost; 
 	if (typeof savegame.bakerys !== "undefined") bakerys = savegame.bakerys; 
 	if (typeof savegame.bakerysCost !== "undefined") bakerysCost = savegame.bakerysCost; 
+	if (typeof savegame.factories !== "undefined") factories = savegame.factories;
+	if (typeof savegame.factoriesCost !== "undefined") factoriesCost = savegame.factoriesCost;
 	reload();
 };
 
@@ -76,6 +90,8 @@ function updateCosts() {
 	document.getElementById("grandmasCost").innerHTML = formatNumber(grandmasCost);
 	bakerysCost = Math.floor(500 * Math.pow(1.3,bakerys));
 	document.getElementById("bakerysCost").innerHTML = formatNumber(bakerysCost);
+	factoriesCost = Math.floor (50000 * Math.pow(1.4,factories));
+	document.getElementById("factoriesCost").innerHTML = formatNumber(factoriesCost);
 };
 
 function updateResources() {
@@ -83,13 +99,15 @@ function updateResources() {
 	document.getElementById("children").innerHTML = formatNumber(children);
 	document.getElementById("grandmas").innerHTML = formatNumber(grandmas);
 	document.getElementById("bakerys").innerHTML = formatNumber(bakerys);
+	document.getElementById("factories").innerHTML = formatNumber(factories);
 };
 
 function updateRates() {
-	document.getElementById("cps").innerHTML = (childrenCps * children) + (grandmasCps * grandmas) + (bakerysCps * bakerys);
+	document.getElementById("cps").innerHTML = formatNumber((childrenCps * children) + (grandmasCps * grandmas) + (bakerysCps * bakerys) + (factoriesCps * factories));
 	document.getElementById("childrenCps").innerHTML = formatNumber(childrenCps);
 	document.getElementById("grandmasCps").innerHTML = formatNumber(grandmasCps);
 	document.getElementById("bakerysCps").innerHTML = formatNumber(bakerysCps);
+	document.getElementById("factoriesCps").innerHTML = formatNumber(factoriesCps);
 };
 
 function reset() {
@@ -104,5 +122,6 @@ window.setInterval(function() {
 	cookieClick(children);
 	cookieClick(5 * grandmas);
 	cookieClick(30 * bakerys);
+	cookieClick(1000 * factories);
 	save();
 }, 1000);
