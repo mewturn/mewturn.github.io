10e3d2cb5cc8069cf975fa376eadc0a817fa11d5
mewturn <pathonice@hotmail.com>
2015-05-21 23:52:15 +0800

    New update
    Adding factories

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 var cookies = 0;
 var children = 0;
 var childrenCost = 10;
+var childrenCps = 1;
+
 var grandmas = 0;
 var grandmasCost = 100;
-var cps = children;
+var grandmasCps = 5;
+
+var factories = 0;
+var factoriesCost = 1000;
+var factoriesCps = 20;
+
+var cps = 0;
 
 function cookieClick(number) {
 	cookies = cookies + number;
@@ -15,12 +23,13 @@ function buyChildren() {
 	if (cookies >= childrenCost) {
 		children = children + 1;
 		cookies = cookies - childrenCost;
+		cps = cps + childrenCps
 		reload();
 	};
 };
 
 function buyGrandmas() {
-	var grandmasCost = Math.floor (100 * Math.pow(1.3,grandmas));
+	var grandmasCost = Math.floor(100 * Math.pow(1.2,grandmas));
 	if (cookies >= grandmasCost) {
 		grandmas = grandmas + 1;
 		cookies = cookies - grandmasCost;
@@ -28,13 +37,27 @@ function buyGrandmas() {
 	};
 };
 
+function buyFactories() {
+	var factoriesCost = Math.floor(1000 * Math.pow(1.35,factories));
+	if (factories >= factoriesCost) {
+		factories = factories + 1;
+		cookies = cookies - factoriesCost;
+		reload();
+	};
+};
+
 function save() {
 	var save = {
 		cookies: cookies,
+		cps: cps,
 		children: children,
 		childrenCost: childrenCost,
+		childrenCps: childrenCps,
 		grandmas: grandmas,
-		grandmasCost: grandmasCost
+		grandmasCost: grandmasCost,
+		grandmasCps: grandmasCps,
+		factories: factories,
+		factoriesCost: factoriesCost,
 	};
 	localStorage.setItem("save",JSON.stringify(save)); 
 };
@@ -45,27 +68,26 @@ function load() {
 	if (typeof savegame.children !== "undefined") children = savegame.children; 
 	if (typeof savegame.childrenCost !== "undefined") childrenCost = savegame.childrenCost; 
 	if (typeof savegame.grandmas !== "undefined") grandmas = savegame.grandmas; 
-	if (typeof savegame.grandmasCost !== "undefined") grandmasCost = savegame.grandmasCost; 
+	if (typeof savegame.grandmasCost !== "undefined") grandmasCost = savegame.grandmasCost;
+	if (typeof savegame.factories !== "undefined") factories = savegame.factories;
+	if (typeof savegame.factoriesCost !== "undefined") factoriesCost = savegame.factoriesCost;
 	reload();
-};
-
-function reload() {
-	updateCosts();
-	updateResources();
-	updateRates();
 };
 
 function updateCosts() {
 	childrenCost = Math.floor(10 * Math.pow(1.1, children));
 	document.getElementById("childrenCost").innerHTML = formatNumber(childrenCost);
-	grandmasCost = Math.floor(100 * Math.pow(1.3,grandmas));
+	grandmasCost = Math.floor(100 * Math.pow(1.2, grandmas));
 	document.getElementById("grandmasCost").innerHTML = formatNumber(grandmasCost);
+	factoriesCost = Math.floor(1000 * Math.pow(1.35,factories));
+	document.getElementById("factoriesCost").innerHTML = formatNumber(factoriesCost);
 };
 
 function updateResources() {
 	document.getElementById("cookies").innerHTML = formatNumber(cookies);
 	document.getElementById("children").innerHTML = formatNumber(children);
 	document.getElementById("grandmas").innerHTML = formatNumber(grandmas);
+	document.getElementById("factories").innerHTML = formatNumber(factories);
 };
 
 function updateRates() {
@@ -74,14 +96,22 @@ function updateRates() {
 
 function reset() {
 	localStorage.removeItem("save");
+	reload();
 };
 
 function formatNumber (num) {
     return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,")
 }
 
+function reload() {
+	updateCosts();
+	updateResources();
+	updateRates();
+};
+
 window.setInterval(function() {
 	cookieClick(children);
 	cookieClick(5 * grandmas);
+	cookieClick(10 * factories);
 	save();
 }, 1000);
