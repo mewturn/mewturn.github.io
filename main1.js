807eb291e2e797b1fbc513531ad8f28bcc6d6b81
limsuixin <suixin_lim@mymail.sutd.edu.sg>
2016-11-23 23:01:05 +0800

    auto update of div

--- main1.js
+++ main1.js
@@ -1,17 +1,32 @@
+var content=["hello", "hi"];
+var flag = true;
+var div = document.createElement('div');
+div.className='fight';
+
 function createDiv(){
 	
 	var ok = true;
+
 	
 	while(ok==true){
-		var div = document.createElement('div');
-		div.className='fight';
 		
-		div.innerHTML +="Fight details";
-		document.getElementsByTagName('body')[0].appendChild(div);
-		ok = false;
+		if (flag==true){
+			div.innerHTML = content[0];
+			document.getElementsByTagName('body')[0].appendChild(div);
+			flag= false;	
+
+		}else{
+			div.innerHTML = content[1];
+			document.getElementsByTagName('body')[0].appendChild(div);
+			flag=true;
+		}
+			
+		ok=false;
 	}
 	
 	
 
 
 }
+
+setInterval('createDiv()', 1000);
