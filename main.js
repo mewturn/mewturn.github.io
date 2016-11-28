138c4163d579b266dc8fb74caba0c4e038a42a32
mewturn <pathonice@hotmail.com>
2016-11-28 14:06:52 +0800

    Added Autosave feature
    1. Added Autosave feature
    2. Automatically loads page based on saved data

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Item variables
 var elem = document.getElementById("myBar");
 var health = 100;
 var currenthealth = health;
@@ -5,6 +6,7 @@ var exp = 0;
 var level = 1;
 var damage = 15;
 
+// "Attack" 
 function move() {
 
 	if (currenthealth > damage) {
@@ -20,10 +22,70 @@ function move() {
 			level++;
 		}
 	}
-		
+	
+	reload();
+	save();
+}
+
+// Save game
+function save() {
+	var save = {
+		health: health,
+		currenthealth: currenthealth,
+		exp: exp,
+		level: level,
+		damage: damage,
+	};
+	localStorage.setItem("save",JSON.stringify(save)); 
+};
+
+// Load game
+function load() {
+	updateSavefile();
+	reload();
+};
+
+function updateSavefile() {	
+	// Savefiles 
+	var savegame = JSON.parse(localStorage.getItem("save")); // Main Savefile
+	
+	// Attribute variables
+	if (typeof savegame.health !== "undefined") health = savegame.health; 
+	if (typeof savegame.currenthealth !== "undefined") currenthealth = savegame.currenthealth; 
+	if (typeof savegame.exp !== "undefined") exp = savegame.exp; 
+	if (typeof savegame.level !== "undefined") level = savegame.level; 
+	if (typeof savegame.damage !== "undefined") damage = savegame.damage; 
+};
+
+function reset() {
+	if (confirm("Are you sure you want to reset? All your progress and savefile will be lost!")) {
+		localStorage.removeItem("save");
+		allZero();
+		reload();
+	}
+	else {
+		reload();
+	}
+};
+
+function allZero() {
+	health = 100;
+	currenthealth = health;
+	exp = 0;
+	level = 1;
+	damage = 15;
+	reload();
+};
+
+function reload() {
 	elem.style.width = (currenthealth/health) * 100 + '%';
 	document.getElementById("label").innerHTML = currenthealth;
 	document.getElementById("myExp").innerHTML = exp;
 	document.getElementById("myDamage").innerHTML = damage;
 	document.getElementById("myLevel").innerHTML = level;
-}
+};
+
+window.setInterval(function() {
+	save();
+	reload();
+}, 1000);
